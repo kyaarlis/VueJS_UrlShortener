b885b5d98ac1ae8f20a2d618cb01cd2dbd04e00a
Karlis Veckagans <veckaganskarlis@gmail.com>
2023-04-19 22:15:45 +0300

    first commit

---
+++ url-shortener_Vue/src/components/Form.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+    const urlName = ref('');
+
+    const handleSubmit = () => {
+      // Handle form submission
+      console.log(`URL: ${urlName.value}`);
+
+</script>
+
+<template>
+    <form @submit.prevent="handleSubmit">
+      <label>
+        URL:
+        <input v-model="urlName" type="text" required>
+      </label>
+   
+      <button type="submit">Submit</button>
+    </form>
+  </template>
+  
+  
