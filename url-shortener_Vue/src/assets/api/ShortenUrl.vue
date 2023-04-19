a57700e6102122ec16c3a78a52b46c6a0343f8c9
Karlis Veckagans <veckaganskarlis@gmail.com>
2023-04-19 22:30:15 +0300

    moving forward

---
+++ url-shortener_Vue/src/assets/api/ShortenUrl.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+
+@Component
+export default class MyComponent extends Vue {
+  async shortenUrl(longUrl: string): Promise<string> {
+    try {
+      const response = await axios.post('https://api.t.ly/v1/shorten', {
+        long_url: longUrl,
+        domain: 't.ly'
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Api-Key': 'q9L9EeAOUA0UX5QWfuoyDhQEQbHnIIJ6xlKoYhnYnYVhKPq5WrAVIQ82q3rX' // Replace with your t.LY API key
+        }
+      });
+      return response.data.short_url;
+    } catch (error) {
+      console.error(error);
+      return '';
+    }
+  }
+}
+</script>
--- url-shortener_Vue/src/components/Form.vue
+++ url-shortener_Vue/src/components/Form.vue
@@ -1,23 +1,30 @@
-<script setup lang="ts">
-import { ref } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ShortenUrl from '../assets/api/ShortenUrl.vue';
 
-    const urlName = ref('');
-
-    const handleSubmit = () => {
-      // Handle form submission
-      console.log(`URL: ${urlName.value}`);
-
+export default defineComponent({
+  data() {
+    return {
+      longUrl: '',
+      shortUrl: ''
+    };
+  },
+  methods: {
+    async onSubmit() {
+      this.shortUrl = await ShortenUrl(this.longUrl);
+    }
+  }
+});
 </script>
 
 <template>
-    <form @submit.prevent="handleSubmit">
-      <label>
-        URL:
-        <input v-model="urlName" type="text" required>
-      </label>
-   
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form @submit.prevent="onSubmit">
+        <label for="long-url">Long URL:</label>
+        <input id="long-url" type="text" v-model="longUrl" required>
+        <button type="submit">Shorten</button>
+      </form>
+      <p v-if="shortUrl">Short URL: <a :href="shortUrl">{{ shortUrl }}</a></p>
+    </div>
   </template>
-  
   
